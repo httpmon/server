48c5ca6ec1371dd29aa3337020f62a11fe611302
raha <elahe.dstn@gmail.com>
2020-07-24 21:15:41 +0430

    Add config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,60 @@
 package config
+
+import (
+	"bytes"
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Database Database `mapstructure:"db"`
+	Nats     Nats     `mapstructure:"nats"`
+}
+
+type Database struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	DBName   string `mapstructure:"dbname"`
+	Password string `mapstructure:"password"`
+	SSLmode  string `mapstructure:"sslmode"`
+}
+
+type Nats struct {
+	Host  string `mapstructure:"host"`
+	Topic string `mapstructure:"topic"`
+	Queue string `mapstructure:"queue"`
+}
+
+func Read() Config {
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+
+	if err := viper.ReadConfig(bytes.NewBufferString(Default)); err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	viper.SetConfigName("config")
+
+	if err := viper.MergeInConfig(); err != nil {
+		log.Print("No config file found")
+	}
+
+	viper.SetEnvPrefix("monitor")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	viper.AutomaticEnv()
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Fatalf("err: %s", err)
+	}
+
+	return cfg
+}
+
+func (d Database) Cstring() string {
+	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s  sslmode=%s",
+		d.Host, d.Port, d.User, d.DBName, d.Password, d.SSLmode)
+}
--- config/default.go
+++ config/default.go
@@ -1,1 +1,15 @@
 package config
+
+const Default = `
+db:
+  host: 127.0.0.1
+  port: "5431"
+  user: postgres
+  dbname: monitor
+  password: postgres
+  sslmode: disable
+nats:
+  host: nats://localhost:4221
+  topic: monitor
+  queue: monitors
+`
